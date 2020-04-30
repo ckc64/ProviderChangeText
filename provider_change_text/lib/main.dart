ea21d9e4868d937aeafa53c50723776ee12aab6b
Charles Kurt <32605261+ckc64@users.noreply.github.com>
2020-04-30 11:43:21 +0800

    Update main.dart

--- provider_change_text/lib/main.dart
+++ provider_change_text/lib/main.dart
@@ -1,48 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider<Data>(
-      create:(context) => Data(),
-        child : MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: MyHomePage(),
-        ),
-    );
+void main() => runApp(MyHomePage());
 
 
-  }
-}
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("SAMPLE PROVIDER TEXT CHANGE"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MyHomePage2(),
-            SizedBox(
-              height: 20,
+    return ChangeNotifierProvider<Data>(
+      create: (context)=>Data(),
+      child: MaterialApp(
+          home:Scaffold(
+            appBar: AppBar(
+              title: Text("SAMPLE PROVIDER TEXT CHANGE"),
             ),
-           MyHomePage1()
-          ],
-        ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  MyHomePage2(),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  MyHomePage1(),
+
+               
+                ],
+              ),
+            ),
+            // This trailing comma makes auto-formatting nicer for build methods.
+          ),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+
   }
 }
 
@@ -62,17 +54,19 @@ class MyHomePage2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextField(
       onChanged: (value) {
-        Provider.of<Data>(context).changeData(value);
+        Provider.of<Data>(context, listen: false).changeData(value);
+        print(value);
       }
     );
   }
 }
 
-class Data extends ChangeNotifier {
+class Data with ChangeNotifier {
   String inputData = "Default Text";
 
   void changeData(String txt) {
     inputData = txt;
+
     notifyListeners();
   }
 }
